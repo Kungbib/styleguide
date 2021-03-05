8decbe57c2ba9df3782949f993361ee08d6c3c81
Ola Blissing <ola.blissing@gmail.com>
2021-03-05 14:17:12 +0100

    Deploy script update to reflect errors

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 source deploy.cfg
 remote="${USERNAME}@${HOST}:${REMOTE_DIR}"
-echo "-> Deploying to ${remote}"
+echo -e "-> Deploying:\n\tUser:\t${USERNAME}\n\tHost:\t${HOST}\n\tPath:\t${REMOTE_DIR}"
+echo -e "\t(${remote})"
 
 deployrsync="rsync -vzcrSLhp ./build/* ${remote}"
 eval $deployrsync
-
-echo "-> Deploy complete"
+exit_code=$?
+if [ "$exit_code" = "0" ] ; then
+    echo "-> Deploy complete!"
+elif [ "$exit_code" = "255" ] ; then
+    echo "[!] Deploy failed, make sure you are connected to the correct network."
+else
+    echo "[!] Deploy failed."
+fi
